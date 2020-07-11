cec8399397511b59ee6c3bd1b8f806e5f0e49a80
Ivan Babushok <babushok@mail.ru>
2020-07-12 02:20:54 +0800

    travis commit 4 gcc

--- test_version.cpp
+++ test_version.cpp
@@ -2,8 +2,8 @@
 
 #include "lib.h"
 
-#include <boost/test/unit_test.hpp>
-//#include <boost/test/included/unit_test.hpp>
+//#include <boost/test/unit_test.hpp>
+#include <boost/test/included/unit_test.hpp>
 
 BOOST_AUTO_TEST_SUITE(helloworld_test_suite)
 
