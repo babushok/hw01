9ef4090ef37be08e0fa54141c892d7db5eb40df4
Ivan Babushok <babushok@mail.ru>
2020-07-12 03:45:31 +0800

    bionic commit 3

--- main.cpp
+++ main.cpp
@@ -2,12 +2,15 @@
 
 #include <spdlog/spdlog.h>
 #include <spdlog/sinks/stdout_sinks.h>
+#include <iostream>
 
 int main()
 {
-    auto logger = spdlog::stdout_logger_mt("console");
-    logger->info("version {} was started", version());
+//    auto logger = spdlog::stdout_logger_mt("console");
+//    logger->info("version {} was started", version());
+
+    std::cout << "build " << version() << std::endl;
+    std::cout << "Hello, World!" << std::endl;
 
     return 0;
-
 }
